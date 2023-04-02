25c03d3b79d1f0444820b26c8553bfe13031e988
codeDenis-tech <67652452+codeDenis-tech@users.noreply.github.com>
2023-04-02 14:56:55 -0400

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -2,8 +2,8 @@ resource "aws_lb" "instance" {
   name               = "alb"
   load_balancer_type = "application"
   subnets = [
-    "subnet-24cdfd6f",
-    "subnet-6a5e4813"
+    "subnet-07adea6ad1ef80f5a",
+    "subnet-070ee9f78c9a86704"
   ]
 }
 
@@ -22,7 +22,7 @@ resource "aws_lb_target_group" "instance" {
   target_type          = "ip"
   protocol             = "HTTP"
   port                 = 8400
-  vpc_id               = "vpc-6e8a0f16"
+  vpc_id               = "vpc-083519d8057ceb271"
   deregistration_delay = 30 // seconds
   health_check {
     interval          = 5 // seconds
